c7f398a3f6846a21d736058ed75176ead4079a46
Julio Berina <julioberina@gmail.com>
2019-08-25 18:48:09 -0700

    Remove extra binds and set bounds to decrement

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,8 @@ export default class App extends React.Component<Props, State> {
 
   decrement() {
     this.setState({
-      count: this.state.count - 1,
-      message: this.state.count - 1 + ' clicks'
+      count: this.state.count > 0 ? this.state.count - 1 : 0,
+      message: (this.state.count > 0 ? this.state.count - 1 : 0) + ' clicks'
     });
   }
 
@@ -48,8 +48,8 @@ export default class App extends React.Component<Props, State> {
         <h1>Counter</h1>
         <p>{this.state.message}</p>
         <button onClick={this.increment}>+1</button>
-        <button onClick={this.decrement.bind(this)}>-1</button>
-        <button onClick={this.reset.bind(this)}>0</button>
+        <button onClick={this.decrement}>-1</button>
+        <button onClick={this.reset}>0</button>
       </section>
     );
   }
